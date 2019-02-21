dea953959c4b7278dc3b65280bde0d77733af510
Emelie Pettersson <emelie.pettersson@ericsson.com>
2019-02-21 11:24:13 +0100

    Combine site + release in separate stages

--- prepare_release.sh
+++ prepare_release.sh
@@ -4,15 +4,11 @@
 # GIT_TAG is sent in to this script from travis
 
 REGEX=^[0-9]+\.[0-9]+\.[0-9]+$      # a regex matching git tags set by developers.
-
 git fetch --tags -q
 GIT_TAG=$(git describe --tags $TRAVIS_COMMIT)    # export the git tag pointing to the current commit
 
-
 echo "Started script"
-echo $GIT_TAG
 
-# First we check if git tag is valid for release
 if [[ $GIT_TAG =~ $REGEX ]]; then
     echo "Generating release notes ..."
     npm install github-release-notes -g
@@ -24,6 +20,6 @@ if [[ $GIT_TAG =~ $REGEX ]]; then
     git diff pom.xml;
 
 else
-    echo "Skipping release because a correct git tag was not found"
+    echo "Skipping release because correct git tag was not found."
     travis_terminate 0;
 fi
